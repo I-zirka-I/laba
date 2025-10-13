03c69fd110be42fefe719cf98655af39323e7052
Гаврилин Александр Александрович ИП-514 <zirkatop@yandex.ru>
2025-10-13 16:42:58 +0700

    Исправил 1 задание

--- src/exercise1.c
+++ src/exercise1.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 int main(int argc, char** argv){
     
-    float arr[10];
-    float sum=0;
-    for(int i=0; i<10;i++){
-        arr[i] = atof(argv[i+1]);
+    double arr[10];
+    double sum=0;
+    for (int i = 0; i < 10; i++){
+        if (scanf("%lf", &arr[i]) != 1)
+            return 1;
         sum += arr[i];
     }
     printf("%.3f\n",sum/10);
