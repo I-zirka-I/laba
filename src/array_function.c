9a834a29595cedba46c84561b59a3c1a94c458b5
Гаврилин Александр Александрович ИП-514 <zirkatop@yandex.ru>
2025-10-19 18:50:10 +0700

    Задания выполнены

--- src/array_function.c
+++ src/array_function.c
@@ -1,8 +1,49 @@
 #include "../include/array_function.h"
 
-void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {}
+void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {
+    for(int i=0;i<size_src1;i++){
+        dest[i]=src1[i];
 
-void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {}
+    }
+    for(int i=0;i<size_src2;i++){
+        dest[size_src1+i]=src2[i];
+    }
+    
+}
 
-void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {}
+void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {
+    for(int i=0;i<size_src1;i++){
+        dest[i]=src1[i];
+
+    }
+    for(int i=0;i<size_src2;i++){
+        dest[size_src1+i]=src2[i];
+    }
+    for(int i=0;i<(size_src1+size_src2)-1;i++){
+        for(int j=0;j<(size_src1+size_src2)-1;j++){
+            if(dest[j]<dest[j+1]){
+                int a = dest[j];
+                dest[j]=dest[j+1];
+                dest[j+1]=a;
+            }
+        }
+    }
+}
+
+void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
+    *min=src[0];
+    *max=src[0];
+    for(int i=0;i<size;i++){
+        if(src[i]>max){
+            max=src[i];
+
+        }
+    }
+    for(int i=0;i<size;i++){
+        if(src[i]<min){
+            min=src[i];
+            
+        }
+    }
+}
 
