3964175a166dc84da550397138b851235479834c
Гаврилин Александр Александрович ИП-514 <zirkatop@yandex.ru>
2025-10-21 16:16:49 +0700

    Дай бог все заработает

--- src/array_function.c
+++ src/array_function.c
@@ -34,13 +34,13 @@ void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
     *min=src[0];
     *max=src[0];
     for(int i=0;i<size;i++){
-        if(src[i]>max){
+        if(src[i]>*max){
             max=src[i];
 
         }
     }
     for(int i=0;i<size;i++){
-        if(src[i]<min){
+        if(src[i]<*min){
             min=src[i];
             
         }
